d304c29300134040f514abc7b0b247f900775ff7
MiricleDev <rohan13k@outlook.com>
2026-07-03 15:07:59 -0500

    Add Spanish grammar lesson 1 vocabulary flashcards

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,9 +6,10 @@ import { demoQuizzes } from "./demo/demoQuizzes";
 import { demoReadings } from "./demo/demoReadings";
 import { demoGrammarLessons } from "./demo/demoGrammar";
 import { spanishGrammarLesson1MasculineFeminine } from "./grammar/spanishGrammarLesson1MasculineFeminine";
+import { spanishGrammarLesson1VocabularyDeck } from "./flashcards/spanishGrammarLesson1Vocabulary";
 import type { ActivityType } from "../types";
 
-export const flashcardDecks = [...demoFlashcardDecks];
+export const flashcardDecks = [...demoFlashcardDecks, spanishGrammarLesson1VocabularyDeck];
 export const sentenceBuilderLessons = [...demoSentenceBuilderLessons];
 export const verbTrainerSets = [...demoVerbSets];
 export const whatsappStories = [...demoStories];
